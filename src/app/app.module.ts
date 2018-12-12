129b04ba213bb11b11063fc8cfd94c348f81e1fc
Alexnguyen17 <Alexnguyen170395@gmail.com>
2018-12-12 15:44:56 +0700

    p

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { ColorPickerPipe } from './color-picker';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // ColorPickerPipe,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule,
